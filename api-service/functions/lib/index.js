136361160e9485951d491083193eda2ccee3c1f6
Alexander <31865557+Alexander-Garcia@users.noreply.github.com>
2025-01-17 17:33:18 -0600

    chore(api): format

--- api-service/functions/lib/index.js
+++ api-service/functions/lib/index.js
@@ -23,13 +23,23 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createUser = void 0;
+exports.generateUploadUrl = exports.createUser = void 0;
 const app_1 = require("firebase-admin/app");
 const firestore_1 = require("firebase-admin/firestore");
 const identity_1 = require("firebase-functions/v2/identity");
 const logger = __importStar(require("firebase-functions/logger"));
+const functions = __importStar(require("firebase-functions"));
+const storage_1 = require("@google-cloud/storage");
+const https_1 = require("firebase-functions/v2/https");
 (0, app_1.initializeApp)();
 const firestore = new firestore_1.Firestore();
+const storage = new storage_1.Storage();
+const rawVideoBucketName = 'ag-yt-raw-videos';
+/**
+ * Create a user in the database when a user is created in Firebase Auth.
+ * @param user - The user object created in Firebase Auth.
+ * @returns void
+ */
 exports.createUser = (0, identity_1.beforeUserCreated)(({ data: user }) => {
     const userInfo = {
         uid: (user === null || user === void 0 ? void 0 : user.uid) || '',
@@ -39,5 +49,24 @@ exports.createUser = (0, identity_1.beforeUserCreated)(({ data: user }) => {
     firestore.collection('users').doc(userInfo.uid).set(userInfo);
     logger.info('Creating user', JSON.stringify(userInfo));
     return;
+});
+exports.generateUploadUrl = (0, https_1.onCall)({ maxInstances: 1 }, async (request) => {
+    // Check if user is auth
+    if (!request.auth) {
+        // https://firebase.google.com/docs/reference/node/firebase.functions
+        // for FunctionsErrorCode strings
+        throw new functions.https.HttpsError('failed-precondition', 'Function must be called while authenticated');
+    }
+    const auth = request.auth;
+    const data = request.data;
+    const bucket = storage.bucket(rawVideoBucketName);
+    // generate unique filename
+    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+    const [url] = await bucket.file(fileName).getSignedUrl({
+        version: 'v4',
+        action: 'write',
+        expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+    });
+    return { url, fileName };
 });
 //# sourceMappingURL=index.js.map
